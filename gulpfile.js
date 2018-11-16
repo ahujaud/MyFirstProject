ba1a589c39083d01937a524d474c945788d53582
uday_ahuja <ahujaud@gmail.com>
2018-11-16 22:52:26 +0530

    Modified gulpfile and added del task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const concat = require('gulp-concat');
-const tsproject = ts.createProject('tsconfig.json')
+const tsproject = ts.createProject('tsconfig.json');
+const del = require('del');
 
-gulp.task('scripts', function (){
-    return gulp.src('Ts/*.ts')
+gulp.task('clean', function clean(){
+    return del('www/')
+})
+gulp.task('scripts',function scriptRunner(){
+    return gulp.src(['**/*.ts','!node_modules/**/*.ts','!node_modules/**/*.js'])
                 .pipe(tsproject())
                 .js.pipe(concat('app.js'))
                 .pipe(gulp.dest('www/js/'));
 });
 
-gulp.task('watch',gulp.series('scripts', function watcher(){
+gulp.task('watch',function watcher(){
     return gulp.watch('Ts/*.ts',gulp.series('scripts'));
-}));
+});
 
-gulp.task('default',gulp.series('watch'));
+gulp.task('default',gulp.series(['clean','scripts','watch']));
